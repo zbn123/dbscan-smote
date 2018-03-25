2c98c34aa8ad3e7af803e189ede74ef4452d0db6
Plapa <lapadsk@hotmail.com>
2018-03-25 11:27:48 +0100

    algorithm now apply clustering

--- dbscan_smote.py
+++ dbscan_smote.py
@@ -1,6 +1,32 @@
 from imblearn.over_sampling.base import  BaseOverSampler
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import MinMaxScaler
 
 
 class DBSCANSMOTE(BaseOverSampler):
+    ''' Clusters the input data using DBScan and then oversamples using smote the defined clusters
+    PARAMETERS:
+    - normalize = whether to normalize the data before clustering
+    - *kwargs = arguments to the passed to the DBSCAN object'''
 
-    def __init__(self, ratio, kind,):
+    _cluster_class = DBSCAN()
+    _normalize = True
+
+    def __init__(self, ratio="auto", random_state=None, normalize = True,  *kwargs):
+
+        super(DBSCANSMOTE, self).__init__(ratio=ratio, random_state=random_state)
+        self._normalize = normalize
+        self._cluster_class = DBSCAN(*kwargs)
+
+    def _fit_clusters(self, X, y=None):
+
+        if self._normalize:
+            min_max = MinMaxScaler()
+            X_ = min_max.fit_transform(X)
+        else:
+            X_ = X
+
+        self._cluster_class.fit(X_, y)
+
+        return self._cluster_class.labels_
+
